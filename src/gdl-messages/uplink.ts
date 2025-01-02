87ac04f2b1fe16fb0aec79911201e4bdcc1d06ae
John Marzulli <john.marzulli@hotmail.com>
2025-01-01 23:46:19 -0800

    Refactor uplink decoding to make more testable.

--- src/gdl-messages/uplink.ts
+++ src/gdl-messages/uplink.ts
@@ -3,7 +3,7 @@ import { LogLevel } from "../logging-object";
 import { DecodedGdl90Message } from "./decoded-gdl90-message";
 import { Gdl90Message } from "./gdl90-message";
 
-export class Frame {
+export class UatUplinkFrame {
     public readonly reserved: number;
     public readonly frameType: number;
     public readonly frame: Uint8Array;
@@ -22,31 +22,9 @@ export class Frame {
 export class Uplink extends DecodedGdl90Message {
     public readonly timeOfReception: number;
     public readonly applicationHeader: Uint8Array;
-    public readonly frames: Frame[] = [];
+    public readonly frames: UatUplinkFrame[] = [];
     public readonly senderLat: number;
     public readonly senderLon: number;
-
-    private decodeHeader(): [number, number] {
-        let rawLat: number = (this.applicationHeader[0] << 15) | (this.applicationHeader[1] << 7) | (this.applicationHeader[2] >> 1);
-        let rawLon: number = ((this.applicationHeader[2] & 0x01) << 23) | (this.applicationHeader[3] << 15) | (this.applicationHeader[4] << 7) | (this.applicationHeader[5] >> 1);
-        let lat: number = rawLat * 360.0 / 16777216.0;
-        let lon: number = rawLon * 360.0 / 16777216.0;
-
-        if (lat > 90) {
-            lat -= 180;
-        }
-        if (lon > 180) {
-            lon -= 360;
-        }
-
-        const appDataValid: boolean = (this.applicationHeader[6] & 0x20) != 0;
-
-        if (!appDataValid) {
-            this.LogError(`valid=${appDataValid}, lat=${lat}, lon=${lon}`);
-        }
-
-        return [lat, lon];
-    }
 
     constructor(
         message: Gdl90Message
@@ -65,51 +43,100 @@ export class Uplink extends DecodedGdl90Message {
 
         this.applicationHeader = payload.slice(0, 8);
 
-        [this.senderLat, this.senderLon] = this.decodeHeader();
+        [this.senderLat, this.senderLon] = decodeHeader(this.applicationHeader);
 
-        let remainingPayload = payload.subarray(8);
-
-        while (remainingPayload.length > 0) {
-            const frameLength = (remainingPayload[0] << 1) | (remainingPayload[1] >> 7);
-
-            const remainingBytes = remainingPayload.length - 2;
-
-            if (remainingBytes < 2 + frameLength) {
-                if (frameLength > 0) {
-                    this.LogError("Hit an overrun of the UAT application data while decoding Uplink message!");
-                }
-
-                break;
-            }
-
-            if (frameLength > 0) {
-                const reserved = (remainingPayload[1] & 0b01110000) >> 4;
-                // Per spec, frame type 0b1111 is reserved for future use
-                // Per spec, frame type 0b0000 is is FIS-B APDU
-                // Per spec, any value from 0b0001 to 0b1110 (inclusive) is reserved for future use
-                const frameType = remainingPayload[1] & 0b00001111;
-
-                if (reserved != 0) {
-                    this.LogError(`Reserved field is not zero: ${reserved}`);
-                }
-
-                if (frameType != 0) {
-                    this.LogError(`Frame type is not zero: ${frameType}`);
-                }
-
-                const frameData: Uint8Array = remainingPayload.subarray(2, frameLength + 2);
-
-                const frame: Frame = new Frame(reserved, frameType, frameData);
-                this.frames.push(frame);
-
-                remainingPayload = remainingPayload.subarray(2 + frameLength);
-            }
-            else {
-                // Obstensibly, we may just consider ending the decoding since it is improbable that we will have more frames
-                remainingPayload = remainingPayload.subarray(2);
-            }
-        }
+        this.frames = getUplinkFrames(payload.slice(8));
 
         this.LogSpew(`UAT UPLINK: lat=${this.senderLat}, long=${this.senderLon}, frameCount=${this.frames.length}`);
     }
+}
+
+function wrapCoordinates(
+    lat: number,
+    lon: number
+): [number, number] {
+    if (lat > 90) {
+        lat -= 180;
+    }
+
+    if (lon > 180) {
+        lon -= 360;
+    }
+
+    return [lat, lon];
+}
+
+function getReservedAndFrameType(
+    payload: Uint8Array
+): [number, number] {
+    const reserved = (payload[1] & 0b01110000) >> 4;
+    // Per spec, frame type 0b1111 is reserved for future use
+    // Per spec, frame type 0b0000 is is FIS-B APDU
+    // Per spec, any value from 0b0001 to 0b1110 (inclusive) is reserved for future use
+    const frameType = payload[1] & 0b00001111;
+
+    if (reserved != 0) {
+        this.LogError(`Reserved field is not zero: ${reserved}`);
+    }
+
+    if (frameType != 0) {
+        this.LogError(`Frame type is not zero: ${frameType}`);
+    }
+
+    return [reserved, frameType];
+}
+
+function getUplinkFrames(
+    payload: Uint8Array
+): UatUplinkFrame[] {
+    let frames: UatUplinkFrame[] = [];
+
+    while (payload.length > 0) {
+        const frameLength = (payload[0] << 1) | (payload[1] >> 7);
+
+        const remainingBytes = payload.length - 2;
+
+        if (remainingBytes < 2 + frameLength) {
+            if (frameLength > 0) {
+                this.LogError("Hit an overrun of the UAT application data while decoding Uplink message!");
+            }
+
+            break;
+        }
+
+        if (frameLength > 0) {
+            let reserved: number;
+            let frameType: number;
+            [reserved, frameType] = getReservedAndFrameType(payload);
+
+            const frameData: Uint8Array = payload.subarray(2, frameLength + 2);
+
+            const frame: UatUplinkFrame = new UatUplinkFrame(reserved, frameType, frameData);
+            frames.push(frame);
+
+            payload = payload.subarray(2 + frameLength);
+        }
+        else {
+            // Obstensibly, we may just consider ending the decoding since it is improbable that we will have more frames
+            payload = payload.subarray(2);
+        }
+    }
+
+    return frames;
+}
+
+function decodeHeader(
+    header: Uint8Array
+): [number, number] {
+    let lat: number = (header[0] << 15) | (header[1] << 7) | (header[2] >> 1);
+    let lon: number = ((header[2] & 0x01) << 23) | (header[3] << 15) | (header[4] << 7) | (header[5] >> 1);
+    lat = lat * 360.0 / 16777216.0;
+    lon = lon * 360.0 / 16777216.0;
+
+    [lat, lon] = wrapCoordinates(lat, lon);
+
+    const appDataValid: boolean = (header[6] & 0x20) != 0;
+    assert(appDataValid);
+
+    return [lat, lon];
 }
